4cdb4db100e7af561d57820a15868929865aeaef
t59407597-maker <t59407597@gmail.com>
2026-09-14 17:19:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from pyrogram import Client, errors
-from pyrogram.session import StringSession
+
 from config import API_HASH, API_ID, SESSION_NAME, SESSION_STRING
 from relay.commands import register
 
+
 if SESSION_STRING:
-    app = Client(StringSession(SESSION_STRING), api_id=API_ID, api_hash=API_HASH)
+    app = Client(
+        SESSION_NAME,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION_STRING,
+    )
 else:
-    app = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH)
+    app = Client(
+        SESSION_NAME,
+        api_id=API_ID,
+        api_hash=API_HASH,
+    )
 
 register(app)
 print("🔊 VC Relay Bot started", flush=True)
